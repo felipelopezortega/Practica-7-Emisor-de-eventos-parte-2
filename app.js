51185f462d23afe9443d1c280b31d8ff13d98465
felipelopezortega <flopezortega@ucol.mx>
2022-03-08 22:09:25 -0600

    all the data is now copied from practice no.6

---
+++ app.js
@@ -0,0 +1,27 @@
+var Emitter = require("./emitter.js");
+
+var emtr = new Emitter();
+
+emtr.on("greet",() =>{
+    console.log("Somewhere, someone said hello.")
+});
+
+emtr.on("greet",()=>{
+    console.log("A greeting ocurred!")
+});
+
+console.log("Hello!");
+emtr.emit("greet");
+
+/* Al tener dos funciones llamadas de la misma manera, al intentar que nos devuelva una con ese nombre, ambas funciones son devueltas
+debido a que el nombre aplica y es el mismo para las dos.*/
+
+emtr.on("jump", ()=>{
+    console.log("someone jumped!");
+});
+
+console.log(emtr);
+emtr.emit("jump");
+
+/* Al imprimir tal cual "emtr", se devuelve el contenido sin ejecución de funciones, además se pueden apreciar en la impresión, los dos
+eventos llamados "greet" que comparten el nombre. */
